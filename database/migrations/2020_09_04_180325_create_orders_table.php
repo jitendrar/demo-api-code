9b35b839cd32169e39051b5fb724d9c4384c5117
Ashok Sadhu <ashok.sadhu@phpdots.com>
2020-09-04 18:07:02 +0530

    order api changes

--- database/migrations/2020_09_04_180325_create_orders_table.php
+++ database/migrations/2020_09_04_180325_create_orders_table.php
@@ -19,12 +19,13 @@ class CreateOrdersTable extends Migration
             $table->integer('address_id')->nullable();
             $table->float('total_price', 11)->nullable();
             $table->double('delivery_charge', 11, 2)->nullable();
-            $table->date('delivery_date')->nullable();
+            $table->dateTime('delivery_date')->nullable();
+            $table->string('delivery_time', 100)->nullable();
             $table->text('special_information')->nullable();
-            $table->integer('order_number')->nullable();
+            $table->string('order_number', 20)->nullable();
             $table->date('actual_delivery_date')->nullable();
+            $table->time('actual_delivery_time')->nullable();
             $table->string('order_status', 50)->nullable();
-            $table->dateTime('actual_delivery_time')->nullable();
             $table->timestamps();
         });
     }
