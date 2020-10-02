d5a55ff660648f01cd3d9cacfbe0b95785543fc1
Ashok Sadhu <ashok.sadhu@phpdots.com>
2020-10-02 11:49:26 +0530

    change in seeder

--- database/seeds/AdminUserTableSeeder.php
+++ database/seeds/AdminUserTableSeeder.php
@@ -1,18 +1,32 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\AdminUser;
 class AdminUserTableSeeder extends Seeder
 {
     public function run()
     {
-    	$row = [
-    		'first_name' => 'Admin',
-    		'last_name' =>'User',
-    		'email' => 'admin@vegetableapp.com',
-    		'password' => bcrypt('123456'),
-    	];
-
-        DB::table('admin_user')->insert($row);
+        $ArrUsers = array(
+            [
+                'id' => 1,
+                'first_name' => 'Admin',
+                'last_name' =>'User',
+                'email' => 'admin@vegetableapp.com',
+                'password' => bcrypt('Password@2'),
+            ],
+            [
+                'id' => 2,
+                'first_name' => 'Ashok',
+                'last_name' =>'Sadhu',
+                'email' => 'ashok.sadhu@phpdots.com',
+                'password' => bcrypt('9067121123'),
+            ]
+        );
+        foreach($ArrUsers as $user) {
+            $isexist = AdminUser::find($user['id']);
+            if (!$isexist) {
+                AdminUser::Create($user);
+            }
+        }
     }
 }
