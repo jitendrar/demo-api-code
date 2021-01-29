b5c3d2d1ee2b0c537950f4d3d6efc52728a84e18
Ashok Sadhu <ashok.sadhu@phpdots.com>
2021-01-29 15:16:19 +0530

    repeat order changes and list product changes

--- app/OfferDetail.php
+++ app/OfferDetail.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\OfferMaster;
 
 class OfferDetail extends Model
 {
@@ -15,4 +16,15 @@ class OfferDetail extends Model
 		return $this->belongsTo('App\Product');
 	}
 	
+	public static function _CheckProductIsInOfferOrNot($product_id=0) {
+        if(!empty($product_id)) {
+            $OfferDetail = OfferDetail::where('product_id',$product_id)->first();
+            if($OfferDetail) {
+            	if(OfferMaster::_CheckOfferIsActve($OfferDetail->offer_master_id)) {
+            		return true;
+            	}
+            }
+        }
+        return false;
+    }
 }
