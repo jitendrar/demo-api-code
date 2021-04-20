df5db4d1a329c96dc100b9806acc122949b42cd5
Ashok Sadhu <admin@phpdots.com>
2021-04-20 18:52:00 +0530

    test

--- app/AdminAction.php
+++ app/AdminAction.php
@@ -1,12 +1,8 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
-
 class AdminAction extends Model
-
 {
     protected $keyType = 'integer';
     protected $table = 'admin_action';
