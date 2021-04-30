8f0aa565adea71087eb8fa32e7ef3ec0465a4230
SejalGauswami <sejal@phpdots.com>
2021-04-30 18:50:48 +0530

    Changed dailyreport list name to Daily Profit Loss Report Added Search form Added Purchase Bill Price, Profit & Loss Column Convert SQL query to laravel query

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -43,7 +43,7 @@ class ReportController extends Controller
 
     public function index() {
         $data = array();
-        $data['module_title'] 	='dailyreports'; 
+        $data['module_title'] 	='Daily Profit Loss Report'; 
         $data['add_url'] 		= route($this->moduleRouteText.'.create');
         $data['addBtnName'] 	= $this->module;
         $data['btnAdd'] 		= 1;
@@ -77,26 +77,32 @@ class ReportController extends Controller
     
     public function Data(Request $request)
     {
-     
+            \DB::enableQueryLog();
+        $modal = WalletHistory::from(function ($query) {
+                $query->select(\DB::raw('DATE(wallet_history.created_at) as bill_date'),\DB::raw('IFNULL(SUM( wallet_history.transaction_amount),0) as collection_amount'))
+                ->from('wallet_history')
+                ->where(\DB::raw('wallet_history.transaction_type'), 'CR')
+                ->whereIn(\DB::raw('wallet_history.transaction_method'), [0,2])
+                ->groupby(\DB::raw('DATE(wallet_history.created_at)'));
+                },'MainWH')->select('MainWH.*',
+                \DB::raw('IFNULL(SUM(billings.total),0) as purchase_bill_amount'),
+                \DB::raw('IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) as refund_amount'),
+                \DB::raw('MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) as total_amount'),
+                \DB::raw('((MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0)) - IFNULL(SUM(billings.total),0)) as profit_loss')
+            )->leftJoin('wallet_history as wallet_history2', function($join)
+            {
+               $join->on(\DB::raw('DATE(wallet_history2.created_at)'), '=', \DB::raw('DATE(MainWH.bill_date)'));
+               $join->where('wallet_history2.transaction_method','=','2');
+            })
+            ->leftJoin('billings as billings',\DB::raw('DATE(billings.bill_date)'),\DB::raw('DATE(MainWH.bill_date)'))
+            ->groupby(\DB::raw('DATE(MainWH.bill_date)'))
+            ->orderby(\DB::raw('DATE(MainWH.bill_date)'),'desc');
 
-        $authUser = Auth::guard('admins')->user();
-        $query = "SELECT MainWH.*, IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) AS refund_amount,
-        MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) AS `total_amount`
-        FROM (
-        SELECT DATE (`wallet_history`.`created_at`) AS bill_date,
-        IFNULL(SUM(`wallet_history`.`transaction_amount`),0) AS collection_amount
-        FROM `wallet_history`
-        WHERE wallet_history.`transaction_type` = 'CR'
-        AND `wallet_history`.`transaction_method` IN(0,2)
-        GROUP BY DATE (`wallet_history`.`created_at`)
-        ) AS MainWH
-        LEFT JOIN `wallet_history` AS `wallet_history2` ON DATE (`wallet_history2`.`created_at`) = MainWH.bill_date AND `wallet_history2`.`transaction_method` = 2
-        GROUP BY MainWH.bill_date
-        ORDER BY MainWH.bill_date DESC";
 
-        $modal = \DB::select($query);
+        
+                        
 
-        return \DataTables::of($modal)
+        return \DataTables::eloquent($modal)
         ->editColumn('refund_amount',function($row){
             return number_format($row->refund_amount,2);
         })
@@ -112,6 +118,36 @@ class ReportController extends Controller
             else
                 return '';
         })
+        ->editColumn('profit_loss', function($row) {
+            if(!empty($row->profit_loss)){
+               if($row->profit_loss>0){
+                $total_profit_loss_amount = 'Profit :: '.$row->profit_loss;
+            }else if($row->profit_loss<0){
+                 $total_profit_loss_amount = 'Loss :: '.abs($row->profit_loss);
+             }
+             return $total_profit_loss_amount;
+            }else{
+                return '';
+            }
+        })
+        ->filter(function ($query) 
+            {
+                $search_start_date  = request()->get("search_start_date");
+                $search_end_date    = request()->get("search_end_date"); 
+                $searchData = array();
+
+                if(!empty($search_start_date)) {
+                    $query = $query->where("MainWH.bill_date", '>=', $search_start_date);
+                    $searchData['search_start_date'] = $search_start_date;
+                }
+                if(!empty($search_end_date)) {
+                    $query = $query->where("MainWH.bill_date", '<=', $search_end_date);
+                    $searchData['search_end_date'] = $search_end_date;
+                }
+
+                $goto = \URL::route($this->moduleRouteText.'.index', $searchData);
+                \session()->put($this->moduleRouteText.'_goto',$goto);
+            })
         ->make(true);
     }
 }
