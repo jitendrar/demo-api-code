47adff37203354acffc2323fd868a6a728543980
SejalGauswami <sejal@phpdots.com>
2021-05-06 14:01:49 +0530

    Daily Report Bill calculation bug in query

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -53,14 +53,16 @@ class ReportController extends Controller
         $total_billing_amount = Billing::sum('total');
 
         $total_collection_amount = WalletHistory::where('transaction_type','CR')->where('transaction_method','0')->sum(\DB::raw('IFNULL((transaction_amount),0)'));
-        $total_pl_amount = $total_collection_amount -  $total_billing_amount;
+        $total_collection_amount_refund = WalletHistory::where('transaction_type','CR')->where('transaction_method','2')->sum(\DB::raw('IFNULL((transaction_amount),0)'));
+        $total_collection_amount_final = $total_collection_amount - $total_collection_amount_refund;
+        $total_pl_amount = $total_collection_amount_final -  $total_billing_amount;
         if($total_pl_amount>0){
             $total_profit_loss_amount = 'Profit :: '.$total_pl_amount;
         }else if($total_pl_amount<0){
              $total_profit_loss_amount = 'Loss :: '.abs($total_pl_amount);
         }
         $data['total_billing_amount'] = $total_billing_amount;
-        $data['total_collection_amount'] = $total_collection_amount;
+        $data['total_collection_amount'] = $total_collection_amount_final;
         $data['total_profit_loss_amount'] = $total_profit_loss_amount ;
 
 
@@ -95,30 +97,36 @@ class ReportController extends Controller
     public function Data(Request $request)
     {
             \DB::enableQueryLog();
-        $modal = WalletHistory::from(function ($query) {
-                $query->select(\DB::raw('DATE(wallet_history.created_at) as bill_date'),\DB::raw('IFNULL(SUM( wallet_history.transaction_amount),0) as collection_amount'))
-                ->from('wallet_history')
-                ->where(\DB::raw('wallet_history.transaction_type'), 'CR')
-                ->whereIn(\DB::raw('wallet_history.transaction_method'), [0,2])
-                ->groupby(\DB::raw('DATE(wallet_history.created_at)'));
-                },'MainWH')->select('MainWH.*',
-                \DB::raw('IFNULL(SUM(billings.total),0) as purchase_bill_amount'),
+       $modal = WalletHistory::from(function ($query) {
+                $query->select('MainWH.*',
                 \DB::raw('IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) as refund_amount'),
-                \DB::raw('MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) as total_amount'),
-                \DB::raw('((MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0)) - IFNULL(SUM(billings.total),0)) as profit_loss')
-            )->leftJoin('wallet_history as wallet_history2', function($join)
-            {
-               $join->on(\DB::raw('DATE(wallet_history2.created_at)'), '=', \DB::raw('DATE(MainWH.bill_date)'));
-               $join->where('wallet_history2.transaction_method','=','2');
-            })
-            ->leftJoin('billings as billings',\DB::raw('DATE(billings.bill_date)'),\DB::raw('DATE(MainWH.bill_date)'))
-            ->groupby(\DB::raw('DATE(MainWH.bill_date)'))
-            ->orderby(\DB::raw('DATE(MainWH.bill_date)'),'desc');
-
-
-        
+                \DB::raw('MainWH.collection_amount - IFNULL(SUM(`wallet_history2`.`transaction_amount`),0) AS total_amount'),
+                'wallet_history2.transaction_amount'
+                )
+                ->from(function ($query) {
+                    $query->select(\DB::raw('DATE(wallet_history.created_at) as bill_date'),\DB::raw('IFNULL(SUM( wallet_history.transaction_amount),0) as collection_amount'))
+                    ->from('wallet_history')
+                    ->where(\DB::raw('wallet_history.transaction_type'), 'CR')
+                    ->where(\DB::raw('wallet_history.transaction_method'),0)
+                    ->groupby(\DB::raw('DATE(wallet_history.created_at)'));
+                
+                },'MainWH')
+                ->leftJoin('wallet_history as wallet_history2', function($join)
+                {
+                    $join->on(\DB::raw('DATE(wallet_history2.created_at)'), '=', \DB::raw('DATE(MainWH.bill_date)'));
+                    $join->where('wallet_history2.transaction_method','=','2');
+                })
+                ->groupby(\DB::raw('DATE(MainWH.bill_date)'));
+                },'MainWHA')
+            ->select('MainWHA.*',
+                \DB::raw('IFNULL(SUM(billings.total),0) as purchase_bill_amount'),
+                \DB::raw('((MainWHA.collection_amount - IFNULL(SUM(`MainWHA`.`transaction_amount`),0)) - IFNULL(SUM(billings.total),0)) as profit_loss')
+            )
+            ->leftJoin('billings as billings',\DB::raw('DATE(billings.bill_date)'),\DB::raw('DATE(MainWHA.bill_date)'))
+            ->groupby(\DB::raw('DATE(MainWHA.bill_date)'),\DB::raw('DATE(billings.bill_date)'))
+            ->orderby(\DB::raw('DATE(MainWHA.bill_date)'),'desc');
                         
-
+// dd(\DB::getQueryLog());
         return \DataTables::eloquent($modal)
         ->editColumn('refund_amount',function($row){
             return number_format($row->refund_amount,2);
@@ -154,11 +162,11 @@ class ReportController extends Controller
                 $searchData = array();
 
                 if(!empty($search_start_date)) {
-                    $query = $query->where("MainWH.bill_date", '>=', $search_start_date);
+                    $query = $query->where("MainWHA.bill_date", '>=', $search_start_date);
                     $searchData['search_start_date'] = $search_start_date;
                 }
                 if(!empty($search_end_date)) {
-                    $query = $query->where("MainWH.bill_date", '<=', $search_end_date);
+                    $query = $query->where("MainWHA.bill_date", '<=', $search_end_date);
                     $searchData['search_end_date'] = $search_end_date;
                 }
 
